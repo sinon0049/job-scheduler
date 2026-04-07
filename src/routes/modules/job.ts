4a6d2b3f793880425ea058e5c3d77d7386104e75
unknown <sinon049@yahoo.com.tw>
2026-04-07 22:36:41 +0800

    feat: establish prisma connection file and job-create route

--- src/routes/modules/job.ts
+++ src/routes/modules/job.ts
@@ -1,4 +1,21 @@
 import express from 'express'
+import { prisma } from '../../lib/prisma.js'
 const router = express.Router()
+
+router.post('/create', async (req, res) => {
+    try {
+        const newJob = await prisma.job.create({
+            data: {
+                run_at: new Date(),
+                type: 'email',
+                payload: 'user@example.com',
+                status: 'PENDING',
+                priority: 1
+            }
+        })
+    } catch (error) {
+        console.log('error creating new job')       
+    }
+})
 
 export default router
