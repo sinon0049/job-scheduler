e16aaf75aa671bc8f9bdc25b093d803133d6b079
unknown <sinon049@yahoo.com.tw>
2026-05-14 22:54:25 +0800

    chore: add port in .env file and add log feature of prisma

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,28 @@
 import express from 'express'
+import 'dotenv/config'
 import { JobServices } from './services/job.services.js'
 import { jobRoutes } from './routes/modules/job.js'
-import { PrismaClient } from '@prisma/client/extension'
+import { PrismaClient } from './generated/prisma/client.js'
 import { PrismaPg } from '@prisma/adapter-pg'
 
+const PORT = process.env.PORT || 3000
 const app = express()
 const prisma = new PrismaClient({
     adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL }),
-    log: ['query']
+    log: [{
+        emit: "event",
+        level: "query"
+    }]
 })
+
+prisma.$on("query", (e) => {
+    console.log("Query: " + e.query)
+    console.log("Params: " + e.params)
+    console.log("Duration: " + e.duration + "ms")
+})
+
 const jobServices = new JobServices(prisma)
 
-jobServices.scanExpiredJobs()
-
 app.use(express.json())
-app.use(jobRoutes(jobServices))
-app.listen(() => console.log('listening'))
+app.use('/job', jobRoutes(jobServices))
+app.listen(PORT, () => console.log('listening'))
