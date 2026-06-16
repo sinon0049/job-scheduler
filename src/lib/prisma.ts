263ced8f5f6c9baff78a3a7c156e3106b1b68564
unknown <sinon049@yahoo.com.tw>
2026-06-17 01:35:56 +0800

    modify: modify handleExpiredJobs and add sleeper/logger

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from "../generated/prisma/client.js";
 import { PrismaPg } from "@prisma/adapter-pg";
+import { context } from "./loggerContext.js";
+
 export const prisma = new PrismaClient({
     adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL }),
     log: [{
@@ -9,7 +11,8 @@ export const prisma = new PrismaClient({
 })
 
 prisma.$on("query", (e) => {
-    console.log("Query: " + e.query)
-    console.log("Params: " + e.params)
-    console.log("Duration: " + e.duration + "ms")
+    console.log(`[${context.getStore()}]`
+    + "Query: " + e.query + "\n" 
+    + "Params: " + e.params + "\n" + 
+    "Duration: " + e.duration + "ms")
 })
--- src/services/job.services.ts
+++ src/services/job.services.ts
@@ -2,6 +2,8 @@ import { Status } from "../generated/prisma/enums.js";
 import type { Job as JobObj } from "../generated/prisma/client.js";
 import type { PrismaClient } from "../generated/prisma/client.js";
 import crypto from 'crypto'
+import { context } from "../lib/loggerContext.js";
+import { sleep } from "../lib/sleep.js";
 
 
 interface CreateJobInput {
@@ -22,19 +24,19 @@ export class JobServices {
 
     scanExpiredJobs = async() => {
         const now = new Date()
-        return await this.db.job.updateManyAndReturn({
-            where: {
-                run_at: {
-                    lte: now
+
+        return await context.run('SCAN', async() => {
+            return await this.db.job.updateManyAndReturn({
+                where: {
+                    run_at: { lte: now },
+                    retry_count: { lt: 3 },
+                    status: Status.PENDING
                 },
-                retry_count: {
-                    lt: 3
+                data: {
+                    status: Status.PROCESSING
                 },
-                status: Status.PENDING
-            },
-            data: {
-                status: Status.PROCESSING
-            }
+                limit: 1000
+            })
         })
     }
 
@@ -44,25 +46,32 @@ export class JobServices {
         let { retry_count } = job
         int < 50 ? isCompleted = true : isCompleted = false
 
-        const result = await this.db.job.update({
-            where: { id: job.id },
-            data: {
-                status: isCompleted ? Status.COMPLETED : retry_count === 2 ? Status.FAILED : Status.PENDING,
-                retry_count: isCompleted ? job.retry_count : job.retry_count + 1
-            }
+        await context.run('PROC', async() => {
+            await this.db.job.update({
+                where: { id: job.id },
+                data: {
+                    status: isCompleted ? Status.COMPLETED : retry_count === 2 ? Status.FAILED : Status.PENDING,
+                    retry_count: isCompleted ? job.retry_count : job.retry_count + 1
+                }
+            })
         })
     }
 
     handleExpiredJobs = async() => {
-        try {
-            const expiredJobs = await this.scanExpiredJobs()
-            if(expiredJobs.length > 0) {
-                await Promise.allSettled(expiredJobs.map((job: JobObj) => this.processJob(job)))
+        while(true) {
+            try {
+                const expiredJobs = await this.scanExpiredJobs()
+                if(expiredJobs.length > 0) {
+                    for(const j of expiredJobs) {
+                        await this.processJob(j)
+                    }
+                    await sleep(1000)
+                } else {
+                    await sleep(5000)
+                }
+            } catch (error) {
+                console.log(error)
             }
-        } catch (error) {
-            console.log(error)
-        } finally {
-            setTimeout(this.handleExpiredJobs, 1000)
-        }
+        }  
     }
 }
